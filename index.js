c501d52933539c899ba38c9b795b9df57ffb72aa
Iryna Koroliuk <irene50288@gmail.com>
2019-04-23 12:53:09 -0700

    small text fix

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ console.log(`1 + 2 = ${Arithmetic.add(1,2)}`);
 console.log(`2 - 1 = ${Arithmetic.subtract(2,1)}`);
 console.log(`2 * 3 = ${Arithmetic.multiply(2,3)}`);
 console.log(`6 / 3 = ${Arithmetic.divide(6,3)}`);
-console.log(`6 / 4 = ${Arithmetic.intDivide(6,4)}`);
-console.log(`6 / 4 = ${Arithmetic.remainder(6,4)}`);
+console.log(`6 div 4 = ${Arithmetic.intDivide(6,4)}`);
+console.log(`6 mod 4 = ${Arithmetic.remainder(6,4)}`);
 
 
 class App extends Component {
