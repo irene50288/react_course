5ceb2d514ea2a7bc7a1e05187b73bac322ef9d58
Iryna Koroliuk <irene50288@gmail.com>
2019-04-23 12:51:54 -0700

    homework 1 task 13

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ import Arithmetic from './Arithmetic'
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
-console.log(`1 + 2 = ${Arithmetic.add(1,2)}`);
-console.log(`2 - 1 = ${Arithmetic.subtract(2,1)}`);
-console.log(`2 * 3 = ${Arithmetic.multiply(2,3)}`);
-console.log(`6 / 3 = ${Arithmetic.divide(6,3)}`);
-console.log(`6 / 4 = ${Arithmetic.intDivide(6,4)}`);
-console.log(`6 / 4 = ${Arithmetic.remainder(6,4)}`);
-
 
 class App extends Component {
     constructor(props) {
@@ -17,7 +10,14 @@ class App extends Component {
 
     render() {
         return (
-            <div>Hello World!</div>
+            <ul>
+                <li>1 + 2 = {Arithmetic.add(1,2)}</li>
+                <li>2 - 1 = {Arithmetic.subtract(2,1)}</li>
+                <li>2 * 3 = {Arithmetic.multiply(2,3)}</li>
+                <li>6 / 3 = {Arithmetic.divide(6,3)}</li>
+                <li>6 div 4 = {Arithmetic.intDivide(6,4)}</li>
+                <li>6 mod 4 = {Arithmetic.remainder(6,4)}</li>
+            </ul>
         )
     }
 
